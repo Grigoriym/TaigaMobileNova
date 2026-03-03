200caca3f3253db33af9c4fe1e295a9f30c7f2c6
g.mikhalchuk <grigoriy.mikhalchuk@deliveryhero.com>
2026-03-03 18:25:17 +0100

    feat: tg-63 ios fix

---
+++ composeApp/src/iosMain/kotlin/com/grappim/taigamobile/di/IosAppModule.kt
@@ -0,0 +1,18 @@
+package com.grappim.taigamobile.di
+
+import com.grappim.taigamobile.core.api.KmpNetworkModule
+import com.grappim.taigamobile.utils.formatter.datetime.DateTimeModule
+import com.grappim.taigamobile.utils.formatter.decimal.DecimalFormatterModule
+import org.koin.core.annotation.Configuration
+import org.koin.core.annotation.Module
+
+@Module(
+    includes = [
+        AppModule::class,
+        KmpNetworkModule::class,
+        DateTimeModule::class,
+        DecimalFormatterModule::class
+    ]
+)
+@Configuration
+class IosAppModule
--- core/storage/src/iosMain/kotlin/com/grappim/taigamobile/core/storage/di/StorageModule.ios.kt
+++ core/storage/src/iosMain/kotlin/com/grappim/taigamobile/core/storage/di/StorageModule.ios.kt
@@ -13,6 +13,7 @@ import com.grappim.taigamobile.core.storage.auth.AuthStorageImpl
 import com.grappim.taigamobile.utils.ui.ColorMapper
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.serialization.json.Json
+import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Configuration
 import org.koin.core.annotation.Module
 import org.koin.core.annotation.Single
@@ -22,9 +23,11 @@ import platform.Foundation.NSUserDomainMask
 
 @Module(includes = [AuthDataStoreModule::class, StorageModule::class])
 @Configuration
+@ComponentScan
 actual class PlatformStorageModule
 
 @Module
+@ComponentScan
 class AuthDataStoreModule {
 
     @Single
